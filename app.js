8b507789fc728b7bb9fdc3b0eaba119db2ea6325
Lavon Green <lavon_green@yahoo.com>
2022-02-13 00:21:17 -0500

    generate HTML File

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+/*const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
@@ -13,3 +13,20 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+
+fs.writeFile('index.html' , generatePage(name, github), err => {
+    if(err) throw new Error(err);
+    console.log('Portfoli complete! Check out index.html to see the output!')
+});
